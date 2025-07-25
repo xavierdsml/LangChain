9d48916c6c1c62ed8c452e58891cba25e74a21dc
xavierdsml <guptaatusharr@gmail.com>
2025-07-25 22:12:45 +0530

    Langchain Prompt updation

---
+++ Langchain Prompt/prompt_generator.py
@@ -0,0 +1,17 @@
+from langchain_core.prompts import PromptTemplate
+
+# template
+template = PromptTemplate(
+    template= """\nPlease summarize the research paper titled \"{paper_input}\" with the following specifications:\nExplanation Style: {style_input}  \nExplanation Length: {length_input}  \n
+    
+    1. Mathematical Details:  \n   - Include relevant mathematical equations if present in the paper.  \n   - Explain the mathematical concepts using simple, intuitive code snippets where applicable.  \n
+    
+    2. Analogies:  \n   - Use relatable analogies to simplify complex ideas.  \nIf certain information is not available in the paper, respond with: \"Insufficient information available\" instead of guessing.  \nEnsure the summary is clear, accurate, and aligned with the provided style and length.\n",
+    "template_format": "f-string",
+    "validate_template": true,
+    "_type": "prompt""",
+
+    input_variables= ['paper_input', 'style_input', 'length_input']
+)
+
+template.save('template.json')
--- Langchain Prompt/prompt_ui.py
+++ Langchain Prompt/prompt_ui.py
@@ -1,6 +1,8 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
-import streamlit as st
+import streamlit as st 
+
+from langchain_core.prompts import PromptTemplate, load_prompt
 load_dotenv()
 
 llm = HuggingFaceEndpoint(
@@ -13,8 +15,26 @@ model = ChatHuggingFace(llm=llm)
 
 #stremlit - webpage
 st.header('Research Tool')
-user_input = st.text_input('Enter your prompt')
+
+paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
+
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+
+# call the template 
+template = load_prompt('template.json')
+
+# fill the placeholder
+prompt = template.invoke(
+  {
+    'paper_input':paper_input,
+    'style_input':style_input,
+    'length_input':length_input
+  }
+)
 
 if st.button('Summarize'):
-  result = model.invoke(user_input)
+  result = model.invoke(prompt)
   st.write(result.content)
